9ac866754f4d39d4ec1d581df6457b552a3b627c
Abdul Ahad <abdultempp@gmail.com>
2024-08-16 11:53:29 +0500

    added login and templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def login_view(request):
+    return render(request, 'login.html')
